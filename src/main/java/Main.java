6e0f74c35869034803413db01cdf57a849276e5f
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-07-13 18:46:28 +0000

    updated print function to include conditional statements to improve formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,27 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    
     //define 1D array with 10 numbers
     int[] HW5arr = {4,5,2,24,98,54,10,8,34,7};
 
+    //set HW5arr equal to arr
+    print(HW5arr);
+    
+  } 
     
   //define print function
-    public static void print(int arr) {
-      for (int i = 0; 1 <arr.length; i++) {
-        System.out.println(arr[i]);
+  public static void print(int[] arr) {
+    for (int i = 0; i <arr.length; i++) {
+      if (i <arr.length -1) {
+        System.out.print(arr[i] + ", ");
+      } else {
+        System.out.print(arr[i]);
+      }
+      
+
       }
     }
   }
 
   
-}
