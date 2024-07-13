e20ae262567fbf2681c587b3eff9f92fb44e9db8
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-07-13 18:23:09 +0000

    define print function for int[] arr using a for-loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,13 @@ public class Main {
     //define 1D array with 10 numbers
     int[] HW5arr = {4,5,2,24,98,54,10,8,34,7};
 
-  
+    
+  //define print function
+    public static void print(int arr) {
+      for (int i = 0; 1 <arr.length; i++) {
+        System.out.println(arr[i]);
+      }
+    }
   }
 
   
