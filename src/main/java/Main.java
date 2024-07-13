a2ee3e760f255ef719a16ddd776edb2d99773d03
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-07-13 18:51:27 +0000

    generalize input for array so that arr = HW5arr

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,11 @@ public class Main {
     int[] HW5arr = {4,5,2,24,98,54,10,8,34,7};
 
     //set HW5arr equal to arr
-    print(HW5arr);
+    int[] arr = HW5arr;
+    print(arr);
     
   } 
-    
+   
   //define print function
   public static void print(int[] arr) {
     for (int i = 0; i <arr.length; i++) {
