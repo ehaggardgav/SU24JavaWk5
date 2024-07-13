63439b181cee5058b90978cdc57d70543e187746
Emmet Haggard <143468532+ehaggardgav@users.noreply.github.com>
2024-07-13 18:12:57 +0000

    define 1D array with 10 numbers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //define 1D array with 10 numbers
+    int[] HW5arr = {4,5,2,24,98,54,10,8,34,7};
+
+  
   }
 
   
